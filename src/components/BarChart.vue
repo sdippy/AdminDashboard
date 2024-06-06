958fabfb4ea2a6f633ae38675f829a854d4ea7a6
Sdippy <strelnikov379@gmail.com>
2024-06-06 20:23:23 +0500

    123

---
+++ src/components/BarChart.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { Chart, registerables } from 'chart.js'
+
+// Регистрируем компоненты Chart.js
+Chart.register(...registerables)
+
+const chartCanvas = ref(null)
+
+onMounted(() => {
+  const ctx = chartCanvas.value.getContext('2d')
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'Dataset 1',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)'],
+          borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)'],
+          borderWidth: 1
+        }
+      ]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  })
+})
+</script>
+
+<template>
+  <div class="h-[300px] w-full mx-auto">
+    <canvas ref="chartCanvas"></canvas>
+  </div>
+</template>
+
+<style scoped>
+.chart-container {
+  position: relative;
+  height: 300px;
+}
+</style>
--- src/components/main_page.vue
+++ src/components/main_page.vue
@@ -1,6 +1,10 @@
+<script setup>
+import BarChart_component from './BarChart.vue'
+</script>
+
 <template>
   <div class="h-svh min-h-[700px] w-full p-10">
-    <div class="w-full h-full">
+    <div class="w-full h-full flex flex-col gap-5">
       <div class="grid grid-cols-3 grid-rows-1 gap-5">
         <div
           class="order flex flex-col h-[140px] rounded-[30px] pl-20 justify-center hover:shadow-2xl hover:-translate-y-1 transition-all ease-in-out"
@@ -20,6 +24,23 @@
           <h2 class="text-[#efefef] font-bold text-[20px]">Клиентов</h2>
           <span class="text-[#efefef] font-bold text-[40px]">20</span>
         </div>
+      </div>
+      <div class="grid grid-cols-3 grid-rows-1 gap-5">
+        <div
+          class="col-span-2 bg-[#2C2C2C] h-[300px] rounded-[30px] hover:shadow-2xl hover:-translate-y-1 transition-all ease-in-out"
+        >
+          <BarChart_component />
+        </div>
+        <div
+          class="col-start-3 h-[300px] bg-[#2C2C2C] rounded-[30px] hover:shadow-2xl hover:-translate-y-1 transition-all ease-in-out"
+        >
+          123
+        </div>
+      </div>
+      <div
+        class="bg-[#2C2C2C] h-full rounded-[30px] hover:shadow-2xl hover:-translate-y-1 transition-all ease-in-out"
+      >
+        123
       </div>
     </div>
   </div>
