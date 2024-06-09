2d024bae2549b2a9e015c8d599e372ea090a4277
Sdippy <strelnikov379@gmail.com>
2024-06-09 20:32:11 +0500

    all_good

--- src/components/review_page.vue
+++ src/components/review_page.vue
@@ -2,6 +2,7 @@
 import { onMounted, ref, provide, reactive, watch } from 'vue'
 import axios from 'axios'
 import debounce from 'lodash.debounce'
+import Datepicker from 'vue3-datepicker'
 
 import CardListReview from './CardListReview.vue'
 import WindowReview from './WindowReview.vue'
@@ -12,6 +13,9 @@ const selectedReview = ref(null) // Переменная для хранения
 
 const searchInput = ref(null)
 const titleSort = ref(null)
+const selectedDate = ref(null)
+const format = 'yyyy-MM-dd'
+const ActiveSelect = ref(null)
 
 const defaultSort = 'id'
 const defaultOrder = 'asc'
@@ -20,7 +24,8 @@ const title = 'product'
 const filters = reactive({
   sort: defaultSort,
   order: defaultOrder,
-  searchQuerry: ''
+  searchQuerry: '',
+  ActiveProcess: ''
 })
 
 const onChangeSelect = (event) => {
@@ -35,12 +40,19 @@ const onChangeSelect = (event) => {
   }
 }
 
+const onChangeActiveProcess = (event) => {
+  filters.ActiveProcess = event.target.value
+}
+
 const clearCategory = async () => {
   searchInput.value.value = ''
   filters.searchQuerry = ''
   filters.sort = defaultSort
   filters.order = defaultOrder
   titleSort.value.value = `${defaultSort}`
+  selectedDate.value = null
+  filters.ActiveProcess = ''
+  ActiveSelect.value.value = ``
 }
 
 const onChangeSearchInput = debounce((event) => {
@@ -81,12 +93,22 @@ provide('review', {
 
 const fetchItems = async () => {
   try {
+    let formattedDate = null
+    if (selectedDate.value) {
+      const date = new Date(selectedDate.value)
+      formattedDate = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`
+    }
     const params = {
       _sort: filters.sort,
-      _order: filters.order
+      _order: filters.order,
+      date: formattedDate
     }
     if (filters.searchQuerry) {
       params.q = filters.searchQuerry
+    }
+
+    if (filters.ActiveProcess) {
+      params.isActive = filters.ActiveProcess
     }
 
     const { data } = await axios.get('http://localhost:3000/reviews', { params })
@@ -108,33 +130,53 @@ onMounted(async () => {
 })
 
 watch(filters, fetchItems)
+watch(selectedDate, fetchItems)
 </script>
 
 <template>
   <WindowReview v-if="reviewWindowOpen" />
   <div class="h-svh min-h-[700px] w-full p-10">
     <div class="w-full h-full flex flex-col gap-5">
-      <div class="w-full flex gap-5 h-[150px] bg-[#383838]">
-        <div class="relative mb-5 h-[40px] w-[400px] flex items-center">
-          <img class="find_icon absolute pl-5" src="/Images/search.svg" alt="Search" />
-          <input
-            ref="searchInput"
-            @input="onChangeSearchInput"
-            class="find_box h-full w-full text-center text-[#efefef] font-light text-[16px] bg-[#2C2C2C] border border-[#383838] rounded-[5px]"
-            placeholder="Поиск"
-            type="text"
-          />
+      <div class="w-full flex gap-5 h-[120px] bg-[#383838]">
+        <div class="w-[400px] flex flex-col">
+          <div class="relative mb-5 w-[400px] flex items-center">
+            <img class="find_icon absolute pl-5" src="/Images/search.svg" alt="Search" />
+            <input
+              ref="searchInput"
+              @input="onChangeSearchInput"
+              class="find_box h-[40px] w-full text-center text-[#efefef] font-light text-[16px] bg-[#2C2C2C] border border-[#383838] rounded-[5px]"
+              placeholder="Поиск"
+              type="text"
+            />
+          </div>
+          <select
+            ref="ActiveSelect"
+            @change="onChangeActiveProcess"
+            class="select_filter h-[40px] w-full text-center text-[#efefef] font-light text-[16px] bg-[#2C2C2C] border border-[#383838] rounded-[5px]"
+          >
+            <option value="">Все статусы</option>
+            <option :value="true">Подтвержден</option>
+            <option :value="false">Не подтвержден</option>
+          </select>
         </div>
-        <div class="h-[40px] w-[400px]">
+        <div class="w-[400px]">
           <select
             ref="titleSort"
             @change="onChangeSelect"
-            class="select_filter h-full w-full text-center text-[#efefef] font-light text-[16px] bg-[#2C2C2C] border border-[#383838] rounded-[5px]"
+            class="select_filter h-[40px] w-full text-center text-[#efefef] font-light text-[16px] bg-[#2C2C2C] border border-[#383838] rounded-[5px]"
           >
             <option value="id">По id (По возрастанию)</option>
             <option :value="title">По наименованию (По возрастанию)</option>
             <option :value="`${title}&_order=desc`">По наименованию (По убыванию)</option>
           </select>
+        </div>
+        <div class="">
+          <datepicker
+            v-model="selectedDate"
+            placeholder="Выберите дату"
+            :format="format"
+            class="h-[40px] w-[400px] text-center text-[#efefef] font-light text-[16px] bg-[#2C2C2C] border border-[#383838] rounded-[5px]"
+          ></datepicker>
         </div>
         <button
           @click="clearCategory"
@@ -145,7 +187,7 @@ watch(filters, fetchItems)
       </div>
 
       <div
-        class="bg-[#2C2C2C] h-[calc(100%-170px)] rounded-[30px] hover:shadow-2xl transition-all ease-in-out flex flex-col p-5 gap-5"
+        class="bg-[#2C2C2C] h-[calc(100%-140px)] rounded-[30px] hover:shadow-2xl transition-all ease-in-out flex flex-col p-5 gap-5"
       >
         <div class="w-full flex justify-between">
           <h2 class="text-[#efefef] font-bold text-[20px]">Отзывы</h2>
